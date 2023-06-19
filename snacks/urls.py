2abda9074e1f3ede959630e173a5281a442bc38e
mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-06-19 13:20:19 +0000

    chore: 🛠️ Edit project settings

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import HomePageView
+from .views import HomePageView,SnackListView,SnackDetailView,SnackCreateView
 
 
 urlpatterns = [
-    path('',HomePageView.as_view(),name="home"),
+    path('',SnackListView.as_view(),name="home"),
+    path('<int:pk>/',SnackDetailView.as_view(), name="snack_detail"),
+    path('create/',SnackCreateView.as_view(), name="snack_create"),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
-
+from .models import Snack
 
 class HomePageView(TemplateView):
     template_name='home.html'
+
+
+class SnackListView(ListView):
+    template_name='snack_list.html'
+    model=Snack
+    context_object_name="snacks"
+
+class SnackDetailView(DetailView):
+    template_name="snack_detail.html"
+    model=Snack 
+
+
+class SnackCreateView(CreateView):
+    template_name="snack_create.html"
+    model=Snack
+    fields="__all__"
+# class SnackUpdateView(UpdateView):
+#     template_name='thing_update.html'
+#     model=Snack
+#     fields="__all__"
+#     success_url=reverse_lazy('')
+
+
+# class SnackDeleteView(DeleteView):
+#     template_name="D"
+#     model=Snack
+#     success_url=reverse_lazy('')
